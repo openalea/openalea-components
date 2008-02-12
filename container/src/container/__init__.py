7541eb0123d6fffb56e2b59e68345af08c244691
Jerome Chopard <jerome.chopard@inria.fr>
2008-02-12 12:54:55 +0000

    back to pure python
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@4360 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- container/src/container/__init__.py
+++ container/src/container/__init__.py
@@ -1,5 +1,5 @@
-from _container import *
+#from _container import *
 #from id_dict import IdDict
 from utils import IdDict
-from graph import Graph as PyGraph
-from topomesh import Topomesh as PyTopomesh
+from graph import Graph
+from topomesh import Topomesh
