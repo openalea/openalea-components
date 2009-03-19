9e85750b9c547b900e73b7c808144ad433b2d516
Jerome Chopard <jerome.chopard@inria.fr>
2009-03-19 16:57:21 +0000

    added txt serialization to topomesh
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@6098 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- container/src/container/__init__.py
+++ container/src/container/__init__.py
@@ -4,7 +4,14 @@ from utils import IdDict
 from graph import Graph
 from grid import Grid
 from relation import Relation
+
+################################
+#
+#	topomesh
+#
+################################
 from topomesh import Topomesh
+from topomesh_txt import write_topomesh,read_topomesh
 from topomesh_algo import clean_geometry,clean_orphans,flip_edge,\
 						expand,border,shrink,\
 						expand_to_border,expand_to_region,external_border,\
