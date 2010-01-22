54b45f1bec3fe9ba93ac9449b3b2f901ee30b1ac
Jerome Chopard <jerome.chopard@inria.fr>
2010-01-22 14:46:05 +0000

    sphinx doc
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@7631 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- svgdraw/setup.py
+++ svgdraw/setup.py
@@ -1,90 +1,66 @@
-
-import sys
-import os
-
 from setuptools import setup, find_packages
-from os.path import join as pj
+from openalea.deploy.metainfo import read_metainfo
 
-build_prefix = "build-scons"
+metadata = read_metainfo('metainfo.ini', verbose=True)
+for key,value in zip(metadata.keys(), metadata.values()):
+    exec("%s = '%s'" % (key, value))
 
+pkg_root_dir = 'src'
+pkgs = [ pkg for pkg in find_packages(pkg_root_dir) if namespace not in pkg]
+top_pkgs = [pkg for pkg in pkgs if len(pkg.split('.')) < 2]
+packages = [ namespace + "." + pkg for pkg in pkgs]
+package_dir = dict( [('',pkg_root_dir)] + [(namespace + "." + pkg, pkg_root_dir + "/" + pkg) for pkg in top_pkgs] )
 
-# Header
-
-# Setup script
-
-# Package name
-name = 'svgdraw'
-namespace = 'openalea'
-pkg_name= namespace + '.' + name
-
-# Package version policy
-version= '0.8.0' 
-
-# Description
-description= 'basic elements to construct tissue models' 
+# Meta information
 long_description= '''
-celltissue contains a main structure tissue and
-interfaces :
-	with physics algorithms
-	with graphical display
-	with read write files
+svgdraw package aims at manipulating
+vectorial graphics keeping close to SVG
+standar. It provide objects and functions
+ - to read write an svg file
+ - to create SVG scene and fill it
+ - to retrieve the geometry of complex
+   geometrical objects like path
 '''
 
-# Author
-author= 'Jerome Chopard'
-author_email= 'jerome.chopard@sophia.inria.fr'
-url= 'http://openalea.gforge.inria.fr'
-license= 'Cecill-C' 
-
-
-
-# Main setup
+# setup function call
+#
 setup(
-    # Meta data
+    # Meta data (no edition needed if you correctly defined the variables above)
     name=name,
     version=version,
     description=description,
     long_description=long_description,
-    author=author,
-    author_email=author_email,
+    author=authors,
+    author_email=authors_email,
     url=url,
     license=license,
     keywords = '',
-
-    
-    # Define what to execute with scons
-    #scons_scripts=['SConstruct'],
-    #scons_parameters=["build","build_prefix="+build_prefix],
-
-    # Packages
+    # package installation
+    packages= packages,
+    package_dir= package_dir,
+    # Namespace packages creation by deploy
     namespace_packages = [namespace],
     create_namespaces = True,
-    packages =  [ 'openalea.' + pkg for pkg in find_packages('src') if namespace not in pkg],
-    package_dir = { 'openalea.svgdraw':  pj('src','svgdraw'), "":"src" }, 
-
-    
-    include_package_data = True,
-    package_data = {'' : ['*.pyd', '*.so', '*.dylib'],},
-
+    # tell setup not  tocreate a zip file but install the egg as a directory (recomended to be set to False)
     zip_safe= False,
-
-    #lib_dirs = {'lib' : pj(build_prefix, 'lib'), },
-    #inc_dirs = { 'include' : pj(build_prefix, 'include') },
-    
-    #postinstall_scripts = ['',],
-
-    # Scripts
-#    entry_points = { 'console_scripts': [
-#                            'fake_script = openalea.fakepackage.amodule:console_script', ],
-#                      'gui_scripts': [
-#                            'fake_gui = openalea.fakepackage.amodule:gui_script',]},
-
     # Dependencies
     setup_requires = ['openalea.deploy'],
-    dependency_links = ['http://openalea.gforge.inria.fr/pi'],
     install_requires = [],
+    dependency_links = ['http://openalea.gforge.inria.fr/pi'],
 
+    # Eventually include data in your package
+    # (flowing is to include all versioned files other than .py)
+    include_package_data = True,
+    # (you can provide an exclusion dictionary named exclude_package_data to remove parasites).
+    # alternatively to global inclusion, list the file to include   
+    package_data = {'' : ['*.pyd', '*.so', '*.zip', '*.png'],},
+
+    # postinstall_scripts = ['',],
+
+    # Declare scripts and wralea as entry_points (extensions) of your package 
+    entry_points = {
+            'wralea': ['celltissue = openalea.celltissue_wralea']
+        },
 
     )
-
 
