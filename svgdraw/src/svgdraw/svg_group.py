df029aa8214e2f52f185c0002820b7e8d07212b8
Jerome Chopard <jerome.chopard@inria.fr>
2010-03-29 12:42:10 +0000

    changed node factory to account for strange svg tools in sodipodi
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@8557 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- svgdraw/src/svgdraw/svg_group.py
+++ svgdraw/src/svgdraw/svg_group.py
@@ -160,56 +160,6 @@ class SVGGroup (SVGElement) :
 	
 	##############################################
 	#
-	#		elements factory
-	#
-	##############################################
-	def svg_element (self, xmlelm) :
-		"""Factory
-		
-		Construct the appropriate SVGElement
-		according to informations stored
-		in an XMLElement
-		
-		:Returns Type: :class:`SVGElement`
-		"""
-		name = xmlelm.nodename()
-		if name[:4] == "svg:" :
-			name = name[4:]
-		
-		if name == "rect" :
-			return SVGBox(0,0,1,1)
-		elif name == "image" :
-			return SVGImage(0,0,1,1,None)
-		elif name == "text" :
-			return SVGText(0,0,"txt")
-		elif name == "circle" :
-			return SVGSphere(0,0,1,1)
-		elif name == "ellipse" :
-			return SVGSphere(0,0,1,1)
-		elif name == "path" :#either a path, a circle or a connector
-			if xmlelm.has_attribute("sodipodi:type") :#may be a circle
-				if xmlelm.attribute("sodipodi:type") == "arc" :#its a circle
-					return SVGSphere(0,0,1,1)
-				else :
-					raise UserWarning("mode not recognized")
-			else : #either a path or a connector
-				if xmlelm.has_attribute("inkscape:connector-type") :#it's a connector
-					return SVGConnector(None,None)
-				else : #it's a simple path
-					return SVGPath()
-		elif name == "g" :#either a group or a layer
-			if xmlelm.has_attribute("inkscape:groupmode") : #either a layer or a stack
-				if xmlelm.has_attribute("descr") and xmlelm.attribute("descr") == "stack" :
-					return SVGStack()
-				else :
-					return SVGLayer("layer",1,1)
-			else :
-				return SVGGroup(1,1)
-		else :
-			return None
-	
-	##############################################
-	#
 	#		xml interface
 	#
 	##############################################
@@ -221,12 +171,14 @@ class SVGGroup (SVGElement) :
 		self._height = read_float(self.get_default("height","0") )
 		#svg elements load
 		for ind in xrange(self.nb_children() ) :
-			svgelm = self.svg_element(self.child(ind) )
-			if svgelm is not None :
+			try :
+				svgelm = svg_element(self.child(ind) )
 				svgelm.from_node(self.child(ind) )
 				self.set_child(ind,svgelm)
 				self._elms.append(svgelm)
 				svgelm.load()
+			except KeyError :
+				pass
 	
 	def save (self) :
 		"""Save SVG attributes as XML attributes
@@ -298,10 +250,5 @@ class SVGLayer (SVGGroup) :
 		self.set_attribute("inkscape:label",self.name() )
 		self.set_attribute("inkscape:groupmode","layer")
 		SVGGroup.save(self)
-
-from xml_element import XMLElement
-from svg_primitive import SVGBox,SVGSphere,SVGImage
-from svg_path import SVGPath,SVGConnector
-from svg_stack import SVGStack
-from svg_text import SVGText
+from svg_factory import svg_element
 
