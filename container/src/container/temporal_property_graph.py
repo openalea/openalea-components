51430b7b1fdaa1ab8dbbfae09b5a323896b289f3
Chakkrit Preuksakarn <pchakkrit@gmail.com>
2012-03-07 10:13:16 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@11791 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- container/src/container/temporal_property_graph.py
+++ container/src/container/temporal_property_graph.py
@@ -186,11 +186,444 @@ class TemporalPropertyGraph(PropertyGraph):
         gp.update(graph.graph)
 
         return g
+    
+    def to_set(s):
+        if not isinstance(s, set):
+            if is instance(s, list):
+                s=set(s)
+            else:
+                s=set([s])
+        return s
+
+    def in_neighbors(self, vid, edge_type=None):
+        """ Return the in vertices of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider (can be a set)
+
+        :Returns:
+        - `neighbors_list` : the set of parent vertices of the vertex vid
+        """
+        if vid not in self :
+            raise InvalidVertex(vid)
+        if edge_type==None:
+            edge_type=set(self.edge_property('edge_type').itervalues())
+        else:
+            edge_type=to_set(edge_type) 
+        tmp=self.edge_property('edge_type')
+        neighbors_list=set()
+        for k, v in tmp.iteritems():
+            if ((v in edge_type) & (self._edges[k][1]==vid)):
+                neighbors_list.add(self._edges[k][0])
+        return neighbors_list
+  
+    def iter_in_neighbors(self, vid, edge_type=None):
+        """ Return the in vertices of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider (can be a set)
+
+        :Returns:
+        - `iterartor` : an iterator on the set of parent vertices of the vertex vid
+        """
+        return iter(self.in_neighbors(vid, edge_type))
+  
+    def out_neighbors(self, vid, edge_type=None):
+        """ Return the out vertices of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider (can be a set)
+
+        :Returns:
+        - `neighbors_list` : the set of child vertices of the vertex vid
+        """
+        if vid not in self :
+            raise InvalidVertex(vid)
+        if edge_type==None:
+            edge_type=set(self.edge_property('edge_type').itervalues())
+        else:
+            edge_type=to_set(edge_type)
+        tmp=self.edge_property('edge_type')
+        neighbors_list=set()
+        for k, v in tmp.iteritems():
+            if ((v in edge_type) & (self._edges[k][0]==vid)):
+                neighbors_list.add(self._edges[k][1])
+        return neighbors_list
+                    
+    def iter_out_neighbors(self, vid, edge_type=None):
+        """ Return the out vertices of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider (can be a set)
+
+        :Returns:
+        - `iterartor` : an iterator on the set of child vertices of the vertex vid
+        """
+        return iter(self.out_neighbors(vid, edge_type))
+  
+    def neighbors(self, vid, edge_type=None):
+        """ Return the neighbors vertices of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider (can be a set)
+
+        :Returns:
+        - `neighbors_list` : the set of neighobrs vertices of the vertex vid
+        """
+        return self.in_neighbors(vid, edge_type) | self.out_neighbors(vid, edge_type)
+
+    def iter_neighbors(self, vid, edge_type=None):
+        """ Return the neighbors vertices of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider (can be a set)
+
+        :Returns:
+        - `iterartor` : iterator on the set of neighobrs vertices of the vertex vid
+        """
+        return iter(self.neighbors(vid, edge_type))
+  
+
+    def in_edges(self, vid, edge_type=None):
+        """ Return in edges of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider (can be a set)
+
+        :Returns:
+        - `edge_list` : the set of the in edges of the vertex vid
+        """
+        if vid not in self :
+            raise InvalidVertex(vid)
+        tmp=self.edge_property('edge_type')
+        if edge_type==None:
+            edge_type=set(self.edge_property('edge_type').itervalues())
+        else:
+            edge_type=to_set(edge_type)
+        edge_list=set()
+        for k , v in tmp.iteritems():
+            if ((v in edge_type) & (self._edges[k][1]==vid)):
+                edge_list.add(k)
+        return  edge_list
+        
+    def iter_in_edges(self, vid, edge_type=None):
+        """ Return in edges of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider (can be a set)
+
+        :Returns:
+        - `iterator` : an iterator on the set of the in edges of the vertex vid
+        """  
+        return iter(self.in_edges(vid, edge_type))
+
+
+    def out_edges(self, vid, edge_type=None):
+        """ Return out edges of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider (can be a set)
+
+        :Returns:
+        - `edge_list` : the set of the out edges of the vertex vid
+        """
+        if vid not in self :
+            raise InvalidVertex(vid)
+        tmp=self.edge_property('edge_type')
+        if edge_type==None:
+            edge_type=set(self.edge_property('edge_type').itervalues())
+        else:
+            edge_type=to_set(edge_type)
+        edge_list=set()
+        for k , v in tmp.iteritems():
+            if ((v in edge_type) & (self._edges[k][0]==vid)):
+                edge_list.add(k)
+        return  edge_list
+
+    def iter_out_edges(self, vid, edge_type=None):
+        """ Return in edges of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider
+
+        :Returns:
+        - `iterator` : an iterator on the set of the in edges of the vertex vid
+        """  
+        return iter(self.out_edges(vid, edge_type))
+
+
+    def edges(self, vid=None, edge_type=None):
+        """ Return edges of the vertex vid
+        If vid=None, return all edges of the graph
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider
+
+        :Returns:
+        - `edge_list` : the set of the edges of the vertex vid
+        """
+        return self.out_edges(vid, edge_type) | self.in_edges(vid, edge_type)
+
+    def iter_edges(self, vid, edge_type=None):
+        """ Return in edges of the vertex vid
+        If vid=None, return all edges of the graph
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider
+
+        :Returns:
+        - `iterator` : an iterator on the set of the edges of the vertex vid
+        """  
+        return iter(self.edges(vid, edge_type))
+
+    
+    def children(self, vid):
+        """ Return children of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+
+        :Returns:
+        - `children_list` : the set of the children of the vertex vid
+        """
+        return self.out_neighbors(vid, 't')
+
+    def iter_children(self, vid):
+        """ Return children of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+
+        :Returns:
+        - `iterator` : an iterator on the set of the children of the vertex vid
+        """
+        return iter(self.children(vid))
+
+    def parent(self, vid):
+        """ Return parents of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+
+        :Returns:
+        - `parents_list` : the set of the parents of the vertex vid
+        """
+        return self.in_neighbors(vid, 't')
+
+    def iter_parent(self, vid):
+        """ Return parents of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+
+        :Returns:
+        - `iterator` : the set of the children of the vertex vid
+        """
+        return iter(self.parent(vid))
+
+    def sibling(self, vid):
+        """ Return sibling of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+
+        :Returns:
+        - `sibling_list` : the set of sibling of the vertex vid
+        """
+        return self.children(self.parent(vid))-set([vid])
+
+    def iter_sibling(self, vid):
+        """ Return of the vertex vid
+        
+        :Parameters:
+        - `vid` : a vertex id
+
+        :Returns:
+        - `iterator` : an iterator on the set of sibling of the vertex vid
+        """
+        return iter(self.sibling(vid))
+
+    
+    def neighborhood(self, vids, n=1, edge_type=None):
+        """ Return the neighborhood of the vertex vid at distance n (the disc, not the circle)
+        
+        :Parameters:
+        - `vid` : a set of vertex id
+
+        :Returns:
+        - `neighbors_list` : the set of the vertices at distance n of the vertex vid
+        """
+        neighbs=set()
+        if n==1 :
+            for vid in vids:
+                neighbs |= (self.neighbors(vid, edge_type) | set([vid]))
+            return neighbs
+        else :
+            for vid in vids :
+                neighbs |= (self.neighborhood(self.neighbors(vid, edge_type), n-1, edge_type) | set([vid]))
+                if list(neighbs)==self._vertices.keys():
+                    return neighbs
+        return neighbs
+
+    def iter_neighborhood(self, vid, n, edge_type=None):
+        """ Return the neighborhood of the vertex vid at distance n (the disc, not the circle)
+        
+        :Parameters:
+        - `vid` : a set of vertex id
+
+        :Returns:
+        - `iterator` : an iterator on the set of the vertices at distance n of the vertex vid
+        """
+        return iter(self.neighborhood(vid, n, edge_type))      
+    
+
+    def descendants(self, vids, n):
+        """ Return the 0, 1, ..., nth descendants of the vertex vid
+        
+        :Parameters:
+        - `vid` : a set of vertex id
+
+        :Returns:
+        - `descendant_list` : the set of the 0, 1, ..., nth descendant of the vertex vid
+        """
+        edge_type='t'
+        neighbs=set()
+        if n==1 :
+            for vid in vids:
+                neighbs |= (self.out_neighbors(vid, 't') | set([vid]))
+            return neighbs
+        else :
+            for vid in vids :
+                neighbs |= (self.descendants(self.out_neighbors(vid, 't'), n-1) | set([vid]))
+                if list(neighbs)==self._vertices.keys():
+                    return neighbs
+        return neighbs
+
+    def iter_descendant(self, vids, n):
+        """ Return the 0, 1, ..., nth descendants of the vertex vid
+        
+        :Parameters:
+        - `vid` : a set of vertex id
+
+        :Returns:
+        - `iterator` : an iterator on the set of the 0, 1, ..., nth descendants of the vertex vid
+        """
+        return iter(self.descendant(vids, n))
+            
+
+    def ancestors(self, vids, n):
+        """ Return the 0, 1, ..., nth ancestors of the vertex vid
+        
+        :Parameters:
+        - `vid` : a set of vertex id
+
+        :Returns:
+        - `anestors_list` : the set of the 0, 1, ..., nth ancestors of the vertex vid
+        """
+        edge_type='t'
+        neighbs=set()
+        if n==1 :
+            for vid in vids:
+                neighbs |= (self.in_neighbors(vid, 't') | set([vid]))
+            return neighbs
+        else :
+            for vid in vids :
+                neighbs |= (self.ancestors(self.in_neighbors(vid, 't'), n-1) | set([vid]))
+                if list(neighbs)==self._vertices.keys():
+                    return neighbs
+        return neighbs
+
+    def iter_ancestors(self, vids, n):
+        """ Return the 0, 1, ..., nth ancestors of the vertex vid
+        
+        :Parameters:
+        - `vid` : a set of vertex id
+
+        :Returns:
+        - `iterator` : an iterator on the set of the 0, 1, ..., nth ancestors of the vertex vid
+        """
+        return iter(self.ancestors(vids, n))
+  
+    def unity_dist(self, vid1, vid2):
+        #if not vid2 in (self._vertices[vid1][0] | self._vertices[vid1][1]):
+         #   print vid1, vid2
+          #  raise InvalidVertex(vid1)
+        return 1
+
+    def topo_dist(self, vid, edge_type=None, func=None):
+        """ Return the distances of each vertices at the vertex vid according a cost function
+        
+        :Parameters:
+        - `vid` : a vertex id
+        - `edges_type` : type of edges we want to consider
+        - `func` : the cost function, if it's None each edge has 1 for cost
+
+        :Returns:
+        - `dist_dict` : a dictionary of the distances, key : vid, value : distance
+        """
+        import numpy as np
+        if func==None :
+            func=self.unity_dist
+        dist={}
+        untreated=set()
+        for k in self._vertices.iterkeys():
+            dist[k]=float('inf')
+            untreated.add(k)
+        #untreated-=set([vid]).
+        treated=set()
+        dist[vid]=0
+        modif=True
+        while (len(untreated)>0):
+            tmpDist=dist.copy()
+            for k in treated:
+                tmpDist.pop(k)
+            actualVid=[k for k in tmpDist.keys()][np.argmin(tmpDist.values())]
+            untreated-=set([actualVid])
+            treated|=set([actualVid])
+            modif=False 
+            for neighb in self.iter_neighbors(actualVid, edge_type):
+                if dist[neighb] > dist[actualVid] + func(neighb, actualVid):
+                    dist[neighb]=dist[actualVid] + func(neighb, actualVid)
+        return dist
+        
+
+    def sub_graph(self, vids):
+        """
+        """
+        #ret=self.copy()
+        
+
+    def group_by(self, func):
+        """ Return a cluster of the vertices according a function
+        
+        :Parameters:
+        - `fund` : the function to make the clusters
+
+        :Returns:
+        - `cluster_dict` : a dictionary of the clusters, key : cluster, value : set of vid in this cluster
+        """
+        ret={}
+        for k in self._vertices.keys():
+            ret.setdefault(func(k)).append(k)
+        return ret
+
 
 def test(display=False):
     import random
     g = TemporalPropertyGraph()
-
+    #
     p1, p2 = PropertyGraph(), PropertyGraph()
     vids = range(1,10)
     edges = []
@@ -200,23 +633,23 @@ def test(display=False):
         for j in l[:3]:
             edges.append((i,j))
     mapping = dict((vid,[vid]) for vid in vids)
-
+    #
     for v in vids:
         p1.add_vertex(v)
         p2.add_vertex(v)
     for s,t in edges:
         p1.add_edge(s,t)
         p2.add_edge(s,t)
-    
+    #
     g.extend([p1,p2],[mapping])
-
-    nxg = g.to_networkx()
-
-    gg = TemporalPropertyGraph().from_networkx(nxg)
-    
-    if display:
-			import matplotlib.pyplot as plt
-			nx.draw(nxg)
-			plt.show()
-
-    return g, gg, nxg
+    #
+    #nxg = g.to_networkx()
+    #
+    #gg = TemporalPropertyGraph().from_networkx(nxg)
+    #
+    #if display:
+    #    import matplotlib.pyplot as plt
+    #    nx.draw(nxg)
+    #    plt.show()
+    #
+    return g
