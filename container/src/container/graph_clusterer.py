bc36d1648656e85bccae0e29a6346244168dc253
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2014-05-12 16:23:58 +0000

    typo debug
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16570 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- container/src/container/graph_clusterer.py
+++ container/src/container/graph_clusterer.py
@@ -603,7 +603,7 @@ class Clusterer:
         # -- Creating the list of vertices:
         if vids is None:
             vids = self.labels
-        elif ids == 'lineaged':
+        elif vids == 'lineaged':
             vids = [k for k in self.labels if k in self.graph.lineaged_vertex(False)]
         elif ids == 'fully lineaged':
             vids = [k for k in self.labels if k in self.graph.lineaged_vertex(True)]
@@ -1364,29 +1364,6 @@ class ClustererChecker:
 
         return vertex_distance2center
 
-def cmap_discretize(cmap, N):
-    """Return a discrete colormap from the continuous colormap cmap.
-
-        cmap: colormap instance, eg. cm.jet. 
-        N: number of colors.
-
-    Example
-        x = resize(arange(100), (5,100))
-        djet = cmap_discretize(cm.jet, 5)
-        imshow(x, cmap=djet)
-    """
-
-    if type(cmap) == str:
-        cmap = plt.get_cmap(cmap)
-    colors_i = np.concatenate((np.linspace(0, 1., N), (0.,0.,0.,0.)))
-    colors_rgba = cmap(colors_i)
-    indices = np.linspace(0, 1., N+1)
-    cdict = {}
-    for ki,key in enumerate(('red','green','blue')):
-        cdict[key] = [ (indices[i], colors_rgba[i-1,ki], colors_rgba[i,ki])
-                       for i in xrange(N+1) ]
-    # Return colormap object.
-    return mcolors.LinearSegmentedColormap(cmap.name + "_%d"%N, cdict, 1024)
 
     def plot_vertex_distance2cluster_center(self, cluster_names=None, print_clustering_name=True, n_colors=None, savefig=None):
         """
