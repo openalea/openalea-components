3c4d7a25f1aa4af0307fad99d6264fea24664be6
Guillaume Baty <guillaume.baty@inria.fr>
2015-04-21 16:13:49 +0200

    openalea.image.pil is now compatible with both PyQt4 and PySide

--- image/src/image/pil/__init__.py
+++ image/src/image/pil/__init__.py
@@ -2,9 +2,12 @@
 __all__ = ['Image', 'ImageQt', 'ImageOps']
 
 try:
-    import Image, ImageQt, ImageOps
+    import Image
+    import ImageOps
 except ImportError:
     try:
-        from PIL import Image, ImageQt, ImageOps
+        from PIL import Image, ImageOps
     except ImportError:
         raise ImportError, "PIL not found. Please install PIL or pillow"
+
+from openalea.image.pil.ImageQt import ImageQt
