40a81c90066b7312732ad95c5e0b06db6e73ccdd
sophie_ribes <sophie.ribes@inria.fr>
2016-09-13 14:07:00 +0200

    trsf interface

---
+++ image/src/image_wralea/trsf_interface.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# -*- python -*-
+#
+#       Copyright 2016 INRIA
+#
+#       File author(s):
+#           Sophie Ribes <sophie.ribes@inria.fr>
+#
+#------------------------------------------------------------------------------
+
+"""Declaration of ITrsf interface
+"""
+
+from openalea.core import *
+try:
+    from timagetk.wrapping.bal_trsf import BalTransformation
+except ImportError:
+    raise ImportError('Import Error')
+
+class ITrsf(IInterface):
+    """Interface for BalTransformation transformations
+    """
+    __pytype__ = BalTransformation
